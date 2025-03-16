f7ff510851b083407046553942863dc1ab887a8f
tasmidur <tasmidurrahman@gmail.com>
2025-03-17 02:16:42 +0600

    update migration

--- prisma/migrations/20250314185835_init/migration.sql
+++ prisma/migrations/20250314185835_init/migration.sql
@@ -325,6 +325,28 @@ CREATE TABLE "Property" (
     CONSTRAINT "Property_pkey" PRIMARY KEY ("Id")
 );
 
+-- AlterTable
+ALTER TABLE "Call" ALTER COLUMN "CreatedAt" DROP DEFAULT;
+
+-- CreateTable
+CREATE TABLE "SpeedDialExtension" (
+    "Id" UUID NOT NULL,
+    "ExtensionId_FK" UUID NOT NULL,
+    "ServiceType" TEXT NOT NULL,
+    "CreatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "UpdatedAt" TIMESTAMP(3),
+    "IsActive" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "SpeedDialExtension_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SpeedDialExtension_ServiceType_ExtensionId_FK_key" ON "SpeedDialExtension"("ServiceType", "ExtensionId_FK");
+
+-- AddForeignKey
+ALTER TABLE "SpeedDialExtension" ADD CONSTRAINT "SpeedDialExtension_ExtensionId_FK_fkey" FOREIGN KEY ("ExtensionId_FK") REFERENCES "Extensions"("Id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_Email_key" ON "User"("Email");
 
